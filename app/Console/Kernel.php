da80165004eeb629aa559eaee76705e3d62a47a9
Alejandro Rodriguez <aleronal@hotmail.com>
2022-04-05 18:24:10 +0100

    Added Task Scheduler to run at 17:22 on the Kernel calling the MailToSendWeeklyController class, senMailWeekly method and worked

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use App\Http\Controllers\MailToSendWeeklyController;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -15,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call([MailToSendWeeklyController::class, 'sendMailWeekly'])->at('17:22');
     }
 
     /**
